9150d344bf9181835e025462f3bf8d78e5371faa
james <jande2@uwo.ca>
2014-04-01 03:08:23 -0400

    In preparation for creation of slider and scrollbar entities

---
+++ 4483.InPeace/src/com/inpeace/entities/ScrollableEntity.java
@@ -0,0 +1,19 @@
+package com.inpeace.entities;
+
+import java.awt.Point;
+
+/**
+ * 
+ * 
+ * @author  James Anderson
+ * @version 1.0
+ * @since   1 Apr 2014
+ */
+public interface ScrollableEntity {
+
+	/**
+	 * @param scroll
+	 */
+	public void scroll(Point scroll);
+	
+}
